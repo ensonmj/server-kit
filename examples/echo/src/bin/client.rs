3a95c574b6dcab68657935be2eb147c93286ff8d
ensonmj <ensonmj@gmail.com>
2022-05-10 21:45:17 +0800

    simplify client

--- examples/echo/src/bin/client.rs
+++ examples/echo/src/bin/client.rs
@@ -1,13 +1,11 @@
 use anyhow::Result;
 use serde_derive::Deserialize;
 use tracing::debug;
-use tracing::instrument;
 
 use server_kit::channel::Channel;
 use server_kit::conf;
 use server_kit::global;
 use server_kit::protocol::nshead::Nshead;
-use server_kit::protocol::Protocol;
 use server_kit::Message;
 
 #[derive(Deserialize)]
@@ -22,34 +20,39 @@ async fn main() -> Result<()> {
     let conf: Conf = conf::read_conf("./conf/client.toml").await?;
     let addr = format!("127.0.0.1:{}", conf.port);
 
-    let channel = Channel::<Nshead>::new(addr);
-    let mut stub = EchoStub::new(channel);
-
+    let mut channel = Channel::<Nshead>::new(addr);
     let req = Message::new(b"hello".to_vec());
-    let resp = stub.echo(req).await?;
+    let resp = channel.process(req).await?;
+
+    // let channel = Channel::<Nshead>::new(addr);
+    // let mut stub = EchoStub::new(channel);
+
+    // let req = Message::new(b"hello".to_vec());
+    // let resp = stub.echo(req).await?;
+
     debug!("Receive data: {resp:?}");
 
     global::teardown();
     Ok(())
 }
 
-struct EchoStub<P>
-where
-    P: Protocol + Sync + Send + 'static,
-{
-    channel: Channel<P>,
-}
+// struct EchoStub<P>
+// where
+//     P: Protocol + Sync + Send + 'static,
+// {
+//     channel: Channel<P>,
+// }
 
-impl<P> EchoStub<P>
-where
-    P: Protocol + Sync + Send + 'static,
-{
-    pub fn new(channel: Channel<P>) -> Self {
-        Self { channel }
-    }
+// impl<P> EchoStub<P>
+// where
+//     P: Protocol + Sync + Send + 'static,
+// {
+//     pub fn new(channel: Channel<P>) -> Self {
+//         Self { channel }
+//     }
 
-    #[instrument(skip_all)]
-    pub async fn echo(&mut self, req: Message) -> server_kit::Result<Vec<u8>> {
-        self.channel.process(req).await
-    }
-}
+//     #[instrument(skip_all)]
+//     pub async fn echo(&mut self, req: Message) -> server_kit::Result<Vec<u8>> {
+//         self.channel.process(req).await
+//     }
+// }
